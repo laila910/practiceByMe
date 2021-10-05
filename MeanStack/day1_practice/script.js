3e4da692537648536bc56c6a2b5e9b613d66577d
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-10-05 17:07:26 +0200

    day1

--- MeanStack/day1_practice/script.js
+++ MeanStack/day1_practice/script.js
@@ -1,23 +1,48 @@
 getData = () => {
-    return JSON.parse(localStorage.getItem(`tasks`)) || [] //that is union in javascript
+    return JSON.parse(localStorage.getItem(`TotalData`)) || [] //that is union in javascript
 }
 setData = (data) => {
-    localStorage.setItem('tasks', JSON.stringify(data))
+    localStorage.setItem('TotalData', JSON.stringify(data))
 
 }
 form = document.querySelector('#form')
 if (form) {
     form.addEventListener('submit', function(e) {
         e.preventDefault()
-        let tasks = getData()
-        let task = {
+        let TotalData = getData()
+        let data = {
             name: this.elements.name.value,
             age: this.elements.age.value,
             salary: this.elements.salary.value
         }
 
-        tasks.push(task)
-        setData(tasks)
+        TotalData.push(data)
+        setData(TotalData)
         this.reset()
+        window.location.href = "allData.html"
+    })
+}
+tbody = document.querySelector('#tbody')
+let TotalData = getData()
+if (tbody) {
+
+    TotalData.forEach((data, index) => {
+        tr = document.createElement('tr')
+        tbody.appendChild(tr)
+        td = document.createElement('td')
+        td.innerText = index
+        tr.appendChild(td)
+        td2 = document.createElement('td')
+        td2.innerText = data.name
+        tr.appendChild(td2)
+        td3 = document.createElement('td')
+        td3.innerText = data.age
+        tr.appendChild(td3)
+        td4 = document.createElement('td')
+        td4.innerText = data.salary
+        tr.appendChild(td4)
+        td5 = document.createElement('td')
+        td5.innerHTML = `<button class="btn btn-warning">Delete</button>`
+        tr.appendChild(td5)
     })
 }
