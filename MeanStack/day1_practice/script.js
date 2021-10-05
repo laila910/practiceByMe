9faaee6498c17a5be4176565e0b050777c4c76a7
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-10-05 16:21:24 +0200

    day1

--- MeanStack/day1_practice/script.js
+++ MeanStack/day1_practice/script.js
@@ -1,10 +1,23 @@
+getData = () => {
+    return JSON.parse(localStorage.getItem(`tasks`)) || [] //that is union in javascript
+}
+setData = (data) => {
+    localStorage.setItem('tasks', JSON.stringify(data))
+
+}
 form = document.querySelector('#form')
-form.addEventListener('submit', function(e) {
-    e.preventDefault()
-    data = {
-        name: this.elements.name.value,
-        age: this.elements.age.value,
-        salary: this.elements.salary.value
-    }
-    console.log(data)
-})
+if (form) {
+    form.addEventListener('submit', function(e) {
+        e.preventDefault()
+        let tasks = getData()
+        let task = {
+            name: this.elements.name.value,
+            age: this.elements.age.value,
+            salary: this.elements.salary.value
+        }
+
+        tasks.push(task)
+        setData(tasks)
+        this.reset()
+    })
+}
