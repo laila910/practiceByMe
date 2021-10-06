858dbedfb57d912183282eea5cf2e2367ab73c83
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-10-06 23:46:52 +0200

    day1

---
+++ php&Laravel/Day1/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// localhost/Day1/
+for ($i = 0; $i < 0; $i++) {
+    for ($y = 0; $y <= $i; $y++) {
+        echo '*';
+    }
+    echo ' <br>';
+}
+//factorial 4
+$num = 4;
+$factorail = 0;
+while ($num >= 1) {
+    $factorial = $num * $num - 1;
+    $num--;
+}
+echo $factorail;
